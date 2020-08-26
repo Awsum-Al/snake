322e9392614dade37d672dab7b316a3c29f947cd
Awsum-Al <awsum_al@hotmail.com>
2020-08-25 18:37:27 -0600

    Add files via upload

--- src/board.tsx
+++ src/board.tsx
@@ -13,6 +13,14 @@ interface SnakeVelocity {
 
 const Board/*: React.FC*/ = () => {
 
+    const [tick, setTick] = React.useState<number>(0);   
+    const [restart, setRestart] = React.useState<boolean>(false);
+
+    // eslint-disable-next-line
+    React.useEffect(() => { initGrid(); placeFood(); const timer = window.setInterval(() => { setTick(prevState => prevState + 1); }, 1000); document.addEventListener('keydown', onKeyDown); return () => { document.removeEventListener('keydown', onKeyDown); window.clearInterval(timer); } }, [restart])
+    // eslint-disable-next-line
+    React.useEffect(() => { moveSnakeHead(); movebody(); gameLogic(); }, [tick])
+
     const [grid, setGrid] = React.useState<Coordinates[][]>();
     const [snakeHead, setsnakeHead] = React.useState<Coordinates>({ col: 10, row: 5 });
     const [tailArray, settailArray] = React.useState<Coordinates[]>([
@@ -21,20 +29,13 @@ const Board/*: React.FC*/ = () => {
         { row: 5, col: 7 }
     ]
     );
-
     const [food, setFood] = React.useState<Coordinates>({ col: -1, row: -1 });
-    const [tick, setTick] = React.useState<number>(0);
     const [snakeVelocity, setsnakeVelocity] = React.useState<SnakeVelocity>({ vx: 0, vy: 0 });
     const [lost, setLost] = React.useState<boolean>(false);
     const [pause, setPause] = React.useState<boolean>(false);
-    const [restart, setRestart] = React.useState<boolean>(false);
     const pauseButton = () => (pause ? [setPause(false), document.addEventListener('keydown', onKeyDown)] : [setPause(true), document.removeEventListener('keydown', onKeyDown), setsnakeVelocity({ vx: 0, vy: 0 })])
-    const restartButton = () => [(setRestart(true), setsnakeHead({row: 5, col: 10}), settailArray([{row: 5, col: 9}, {row: 5, col: 8}, {row: 5, col: 7}]), setLost(false), setsnakeVelocity({vx: 0, vy: 0}), setTick(0))]
-    
-    // eslint-disable-next-line
-    React.useEffect(() => { initGrid(); placeFood(); const timer = window.setInterval(() => { setTick(prevState => prevState + 1); }, 1000); document.addEventListener('keydown', onKeyDown); return () => { document.removeEventListener('keydown', onKeyDown); window.clearInterval(timer); } }, [restart])
-    // eslint-disable-next-line
-    React.useEffect(() => { moveSnakeHead(); movebody(); gameLogic();}, [tick])
+    const restartButton = () => [(setRestart(true), setsnakeHead({ row: 5, col: 10 }), settailArray([{ row: 5, col: 9 }, { row: 5, col: 8 }, { row: 5, col: 7 }]), setLost(false), setsnakeVelocity({ vx: 0, vy: 0 }), setTick(0))]
+    const nextsnakeHead = { row: snakeHead.row + snakeVelocity.vy, col: snakeHead.col + snakeVelocity.vx }
 
     const placeFood = () => {
         setFood({
@@ -76,7 +77,6 @@ const Board/*: React.FC*/ = () => {
         )
     }
 
-
     const getObjectStyleInCell = (loc: Coordinates) => {
         let tempClass: string = ''
         let x = loc.row
@@ -104,7 +104,7 @@ const Board/*: React.FC*/ = () => {
     // Set the snake velocity. Origin is top left 
     const onKeyDown = React.useCallback(event => {
         const { key } = event;
-        
+
         if (key === "ArrowLeft") {
             setsnakeVelocity({ vx: -1, vy: 0 })
         }
@@ -118,14 +118,10 @@ const Board/*: React.FC*/ = () => {
             setsnakeVelocity({ vx: 0, vy: 1 })
         }
     }, []);
-	
-	const nextsnakeHead = {row: snakeHead.row + snakeVelocity.vy, col: snakeHead.col + snakeVelocity.vx}	
 
     const moveSnakeHead = () => {
         if (pause) { setsnakeVelocity({ vx: 0, vy: 0 }) }
         if (snakeVelocity.vx !== 0 || snakeVelocity.vy !== 0) {
-//            let x = snakeHead.row + snakeVelocity.vy
-//            let y = snakeHead.col + snakeVelocity.vx
             setsnakeHead(nextsnakeHead)
         }
     }
@@ -134,13 +130,12 @@ const Board/*: React.FC*/ = () => {
             let tempbody = tailArray.map(z => Object.assign({}, z));
             tempbody.unshift(snakeHead)
             tempbody.pop()
-            //  temptail2 = tempbody.map(z => Object.assign({}, z));
             settailArray(tempbody)
         }
     }
     const gameLogic = () => {
-        let x = nextsnakeHead.row
-        let y = nextsnakeHead.col
+        let x = snakeHead.row
+        let y = snakeHead.col
         let temptail = tailArray.map(z => Object.assign({}, z));
         // The shift removes the first tail section. This avoids collision detections while going in a straight line.
         // temptail.shift()
@@ -151,7 +146,7 @@ const Board/*: React.FC*/ = () => {
             placeFood()
         }
         // #2 The Wall
-        else if (nextsnakeHead.row < 0 || nextsnakeHead.row > 50 || nextsnakeHead.col < 0 || nextsnakeHead.col > 50) {
+        else if (nextsnakeHead.row < 0 || nextsnakeHead.row > 49 || nextsnakeHead.col < 0 || nextsnakeHead.col > 49) {
             console.log('You hit the wall')
             setLost(true)
         }
@@ -164,41 +159,41 @@ const Board/*: React.FC*/ = () => {
 
     let snake_grid = (
 
-            <div>
-                <div className='board-background'>
-                    {grid && drawGrid()}
-                </div>
-                <button className='button' onClick={() => { pauseButton(); }} id='one'>
-                    {pause ? 'Go' : 'Pause'}
-                </button>
-                <button className='button' onClick={() => { restartButton(); }} id='two'>
-                    Restart
-                </button>
-                <button className='button' onClick={() =>window.location.href = ('https://pointerpointer.com/')} id='three'>
-                    Quit
-                </button>
+        <div>
+            <div className='board-background'>
+                {grid && drawGrid()}
             </div>
+            <button className='button' onClick={() => { pauseButton(); }} id='one'>
+                {pause ? 'Go' : 'Pause'}
+            </button>
+            <button className='button' onClick={() => { restartButton(); }} id='two'>
+                Restart
+                </button>
+            <button className='button' onClick={() => window.location.href = ('https://pointerpointer.com/')} id='three'>
+                Quit
+                </button>
+        </div>
 
     );
 
     let game_over = (
 
 
-            <div id="GO">
+        <div id="GO">
 
-                <div className="gameover">
-                    <p> GAME </p>
-                    <p> OVER </p>
-                </div>
-
-                <div className="continue"> <p> CONTINUE? </p> </div>
-
-                <div className="button2">
-                    <div className="yes" onClick={() => { restartButton(); }}> <b>YES</b> </div>
-                    <div className="no"> <a href='https://pointerpointer.com/'> NO </a> </div>
-                </div>
-
+            <div className="gameover">
+                <p> GAME </p>
+                <p> OVER </p>
             </div>
+
+            <div className="continue"> <p> CONTINUE? </p> </div>
+
+            <div className="button2">
+                <div className="yes" onClick={() => { restartButton(); }}> <b>YES</b> </div>
+                <div className="no"> <a href='https://pointerpointer.com/'> NO </a> </div>
+            </div>
+
+        </div>
 
 
     )
